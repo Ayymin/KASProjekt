d0ccd90424021d3d4a3a68fb1196c3edb41cdf1a
Kasper111 <62259040+Kasper111@users.noreply.github.com>
2022-12-02 18:03:20 +0100

    aet

--- KASProjekt/KAS/src/gui/App.java
+++ KASProjekt/KAS/src/gui/App.java
@@ -5,12 +5,14 @@ import storage.Storage;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("aefaeaefaefaer");
         Storage.initContent();
         Application.launch(StartWindow.class);
 
         //SUUUUU
         //aefgaef
         //aefaefaeg
+
 
     }
 }
